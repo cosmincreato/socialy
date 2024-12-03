961af7d1a76bea28b816cb0c6943540b7ec90fae
Mărășoiu Cosmin <marasoiu.cos@gmail.com>
2024-12-03 22:08:24 +0200

    post comment models

---
+++ DAW/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DAW.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public string Label { get; set; }
+        public DateTime Date { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+
+        public virtual ICollection<Comment>? Comments { get; set; }
+
+    }
+}
