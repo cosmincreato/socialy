a7a1df5c565339578cc37439bbc2d1a41316e584
TheFluffyBoi2 <dumitrusebastian2004@gmail.com>
2024-12-03 19:23:03 +0200

    added user

---
+++ DAW/Models/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAW.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Bio {  get; set; }
+
+        [Required(ErrorMessage = "First name required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "You must choose a visibility option")]
+        public bool IsPublic { get; set; }
+    }
+}
