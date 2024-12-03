9491c27f2b9ebd2f0264f2e1b323b0e30b4a0c8c
TheFluffyBoi2 <dumitrusebastian2004@gmail.com>
2024-12-03 18:27:29 +0200

    Add project files.

---
+++ DAW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DAW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
