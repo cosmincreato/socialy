74b913a55993d75b8c4ce250df7b7704bf305022
TheFluffyBoi2 <dumitrusebastian2004@gmail.com>
2024-12-04 14:20:48 +0200

    group-grouppost

---
+++ DAW/Models/Group.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAW.Models
+{
+    public class Group
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Label { get; set; }
+
+        public string? UserId { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
